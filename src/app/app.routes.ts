ba91bef77f613fed015b5f1aed062bda6b8ade30
Paula Gonzalez <pau030492@gmail.com>
2025-08-24 13:34:54 -0300

    Navegacion rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"login",
+        loadComponent: () => import('./login/login').then(login => login.Login),
+    },
+        {
+        path:"registro",
+        loadComponent: () => import('./registro/registro').then(registro => registro.Registro),
+    },
+        {
+        path:"bienvenida",
+        loadComponent: () => import('./bienvenida/bienvenida').then(b => b.Bienvenida),
+    },
+    {
+        path:'',
+        redirectTo: 'bienvenida',
+        pathMatch: 'full'
+    },
+    {
+        path:"error",
+        loadComponent: () => import('./error/error').then(e => e.Error),
+    },
+        {
+        path:"sobre-mi",
+        loadComponent: () => import('./sobre-mi/sobre-mi').then(m => m.SobreMi),
+    }
+];
