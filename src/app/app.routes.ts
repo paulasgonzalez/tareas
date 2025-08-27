5f0cfe5fcddf480c7e00dcc693279cd6b204c428
Paula Gonzalez <pau030492@gmail.com>
2025-08-27 17:17:32 -0300

    Agrego carpeta pages. Mejoro rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,32 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path:"login",
-        loadComponent: () => import('./login/login').then(login => login.Login),
-    },
-        {
-        path:"registro",
-        loadComponent: () => import('./registro/registro').then(registro => registro.Registro),
-    },
-        {
-        path:"bienvenida",
-        loadComponent: () => import('./bienvenida/bienvenida').then(b => b.Bienvenida),
-    },
-    {
         path:'',
         redirectTo: 'bienvenida',
         pathMatch: 'full'
     },
     {
+        path:"login",
+        loadComponent: () => import('./pages/login/login').then(login => login.Login),
+    },
+        {
+        path:"registro",
+        loadComponent: () => import('./pages/registro/registro').then(registro => registro.Registro),
+    },
+        {
+        path:"bienvenida",
+        loadComponent: () => import('./pages/bienvenida/bienvenida').then(b => b.Bienvenida),
+    },
+    {
         path:"error",
-        loadComponent: () => import('./error/error').then(e => e.Error),
+        loadComponent: () => import('./pages/error/error').then(e => e.Error),
     },
         {
         path:"sobre-mi",
-        loadComponent: () => import('./sobre-mi/sobre-mi').then(m => m.SobreMi),
+        loadComponent: () => import('./pages/sobre-mi/sobre-mi').then(m => m.SobreMi),
+    },
+    {
+        path:'**', //ruta comodin siempre al final
+        loadComponent: () => import('./pages/error/error').then(e => e.Error)
     }
 ];
